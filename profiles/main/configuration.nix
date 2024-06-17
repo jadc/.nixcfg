5cd1bdb9d9bc743defc97e42376c127e4e8afa41
jadc <chehimj@gmail.com>
2024-06-16 18:34:14 -0600

    explicit config parent

--- profiles/main/configuration.nix
+++ profiles/main/configuration.nix
@@ -6,75 +6,77 @@
             ./hardware-configuration.nix
         ];
 
-    # Bootloader
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/vda";
-    boot.loader.grub.useOSProber = true;
+    config = {
+        # Bootloader
+        boot.loader.grub.enable = true;
+        boot.loader.grub.device = "/dev/vda";
+        boot.loader.grub.useOSProber = true;
 
-    # Hostname
-    networking.hostName = "jadc";
+        # Hostname
+        networking.hostName = "jadc";
 
-    # Enable networking
-    networking.networkmanager.enable = true;
+        # Enable networking
+        networking.networkmanager.enable = true;
 
-    # Time zone
-    time.timeZone = "America/Edmonton";
+        # Time zone
+        time.timeZone = "America/Edmonton";
 
-    # Locale
-    i18n.defaultLocale = "en_CA.UTF-8";
+        # Locale
+        i18n.defaultLocale = "en_CA.UTF-8";
 
-    # Keymap in X11
-    services.xserver.xkb = {
-        layout = "us";
-        variant = "";
+        # Keymap in X11
+        services.xserver.xkb = {
+            layout = "us";
+            variant = "";
+        };
+
+        # Create user
+        users.users.jad = {
+            isNormalUser = true;
+            description = "jad";
+            extraGroups = [ "networkmanager" "wheel" ];
+            packages = with pkgs; [];
+        };
+
+        # Enable automatic login for the user.
+        services.getty.autologinUser = "jad";
+
+        # Set default shell to zsh for all users
+        environment.shells = with pkgs; [ zsh ];
+        users.defaultUserShell = pkgs.zsh;
+        programs.zsh.enable = true;
+
+        # List packages installed in system profile. To search, run:
+        # $ nix search wget
+        environment.systemPackages = with pkgs; [
+            vim
+            wget
+            git
+        ];
+
+        # Some programs need SUID wrappers, can be configured further or are
+        # started in user sessions.
+        # programs.mtr.enable = true;
+        # programs.gnupg.agent = {
+        #     enable = true;
+        #     enableSSHSupport = true;
+        # };
+
+        # List services that you want to enable:
+
+        # Enable the OpenSSH daemon.
+        # services.openssh.enable = true;
+
+        # Open ports in the firewall.
+        # networking.firewall.allowedTCPPorts = [ ... ];
+        # networking.firewall.allowedUDPPorts = [ ... ];
+        # Or disable the firewall altogether.
+        # networking.firewall.enable = false;
+
+        # Enable flakes
+        nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+        # Do not need to update
+        system.stateVersion = "24.05";
     };
-
-    # Create user
-    users.users.jad = {
-        isNormalUser = true;
-        description = "jad";
-        extraGroups = [ "networkmanager" "wheel" ];
-        packages = with pkgs; [];
-    };
-
-    # Enable automatic login for the user.
-    services.getty.autologinUser = "jad";
-
-    # Set default shell to zsh for all users
-    environment.shells = with pkgs; [ zsh ];
-    users.defaultUserShell = pkgs.zsh;
-    programs.zsh.enable = true;
-
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment.systemPackages = with pkgs; [
-         vim
-         wget
-         git
-    ];
-
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #     enable = true;
-    #     enableSSHSupport = true;
-    # };
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
-
-    # Enable flakes
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-    # Do not need to update
-    system.stateVersion = "24.05";
 }
