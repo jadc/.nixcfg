b01ee9d948a8117b40cd9d3e2b32231f07af28e8
jadc <chehimj@gmail.com>
2024-06-17 16:30:26 -0600

    aliases

--- user/shells.nix
+++ user/shells.nix
@@ -2,7 +2,25 @@
 
 let
     shellAliases = {
-        cat = "bat";
+        # Default flags
+        sudo="sudo -E";
+        du="du -h";
+        cp="cp -ivp";
+        mv="mv -iv";
+        mkdir="mkdir -pv";
+        make="make -j$(nproc)";
+        rsync="rsync -avhP --no-compress";
+
+        ## Colors
+        ls="ls -hN --color=auto --group-directories-first";
+        grep="grep --color=auto";
+        fgrep="fgrep --color=auto";
+        egrep="egrep --color=auto";
+        diff="diff --color=auto";
+
+        # Shortcuts
+        wavtoflac="flac --best --delete-input-file -e -p -V -f --keep-foreign-metadata *.wav";
+        texview="latexmk -pdf -pvc *.tex";
     };
 in
 {
