66c3470315329345f00bc3e2099d701834d34467
jadc <chehimj@gmail.com>
2024-06-24 12:33:38 -0600

    unnecessary path

--- user/scripts/enable.nix
+++ user/scripts/enable.nix
@@ -3,6 +3,6 @@
 {
     # Adds the scripts/bin directory to the user PATH
     config.home.packages = [ 
-        (pkgs.buildEnv { name = "scripts"; paths = [ ./../../user/scripts ]; }) 
+        (pkgs.buildEnv { name = "scripts"; paths = [ ./. ]; }) 
     ];
 }
