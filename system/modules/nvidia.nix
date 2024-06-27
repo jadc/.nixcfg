e35af3bac3f90625ef89b11ab6eb881198fca10e
jadc <chehimj@gmail.com>
2024-06-26 22:44:26 -0600

    nvidia ii

--- system/modules/nvidia.nix
+++ system/modules/nvidia.nix
@@ -7,8 +7,13 @@
         extraPackages = [ pkgs.nvidia_x11 ];
     };
     services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
-    hardware.nvidia.modesetting.enable = true;
     boot.kernelParams = [ "nvidia-drm.modeset=1" ];
-    modesetting.enable = true;
+    hardware.nvidia = {
+        modesetting.enable = true;
+        nvidiaSettings = true;
+        open = false;
+
+        # Use beta channel
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
+    };
 }
