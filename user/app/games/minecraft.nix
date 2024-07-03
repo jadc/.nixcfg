1039f9dbb19dc4b65c016a691eaa71af9e47ac9b
jadc <chehimj@gmail.com>
2024-07-02 18:38:00 -0600

    java conflicts resolved

--- user/app/games/minecraft.nix
+++ user/app/games/minecraft.nix
@@ -2,9 +2,26 @@
 
 { pkgs, ... }:
 
+let
+    alternative = with pkgs; [ jdk8 ];
+in
 {
-    # TODO: use wrapProgram to install jre8 as well without conflicting
-    home.packages = with pkgs; [ prismlauncher jre_minimal ];
+    home.packages = with pkgs; [ prismlauncher ];
+
+    # Java
+    programs.java = {
+        enable = true;
+        
+        # Default java is latest
+        package = pkgs.jdk;
+    };
+
+    # Alternative java versions
+    home.file = (builtins.listToAttrs (builtins.map (jdk: {
+        name = ".jdks/${jdk.version}";
+        value = { source = jdk; };
+    }) alternative));
+
     home.sessionVariables = {
         _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
         _JAVA_AWT_WM_NONREPARENTING = 1;
