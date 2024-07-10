abf51ef3231c860d85a92efb3204ee3439c411a4
jadc <chehimj@gmail.com>
2024-07-09 20:57:58 -0600

    fixed zoxide

--- user/cli/zoxide.nix
+++ user/cli/zoxide.nix
@@ -4,10 +4,7 @@
     programs.zoxide = {
         enable = true;
 
-        # Prevent creation of z and zi commands
-        options = [ "--no-cmd" ];
-
         # Replace cd with zoxide
-        # TODO: figure out how to do that :p
+        options = [ "--cmd cd" ];
     };
 }
