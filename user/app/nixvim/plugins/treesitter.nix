578af178e6cb3b0881cfa6ce70cfca97c23cda40
jadc <chehimj@gmail.com>
2024-07-09 20:10:16 -0600

    name update

--- user/app/nixvim/plugins/treesitter.nix
+++ user/app/nixvim/plugins/treesitter.nix
@@ -2,7 +2,7 @@
     programs.nixvim.plugins.treesitter = {
         enable = true;
         folding = true;
-        indent = true;
+        settings.indent.enable = true;
         nixvimInjections = true;
     };
 }
