209a51ce75d1706310b536db6846b4b9ccef5dd6
jadc <chehimj@gmail.com>
2024-07-11 21:12:49 -0600

    work profile

---
+++ config/work/home.nix
@@ -0,0 +1,15 @@
+{
+    imports = 
+        [
+            ./../../user/cli/shells.nix
+            ./../../user/cli/git.nix
+            ./../../user/cli/zoxide.nix
+            ./../../user/cli/eza.nix
+            ./../../user/cli/fzf.nix
+            ./../../user/cli/nixvim
+            ./../../user/cli/tmux.nix
+            ./../../user/cli/archivers.nix
+            ./../../user/cli/rsync.nix
+            ./../../user/cli/ripgrep.nix
+        ];
+}
