3136824ec988a55ccc8d6d4502d77923e5710f61
jadc <chehimj@gmail.com>
2024-07-15 22:28:27 -0600

    starting config

--- config/mac/configuration.nix
+++ config/mac/configuration.nix
@@ -5,6 +5,8 @@
         [
             # System configuration
             ./../../system/users.nix
+            ./../../system/nix-gc.nix
+            ./../../system/fonts.nix
         ];
 
     # Enable Nix daemon for macOS
@@ -19,6 +21,66 @@
 
     # Allow Touch ID to grant superuser
     security.pam.enableSudoTouchIdAuth = true;
+
+    system.defaults = {
+        dock = {
+            autohide = true;
+            show-recents = false;
+        };
+
+        finder = {
+            # Show full path in finder title
+            _FXShowPosixPathInTitle = true;
+            # Show all file extensions
+            AppleShowAllExtensions = true;
+            # Disable warning when changing file extension
+            FXEnableExtensionChangeWarning = false;
+            # Enable quit menu item
+            QuitMenuItem = true;
+            # Show path bar
+            ShowPathbar = true;
+            # Show status bar
+            ShowStatusBar = true;
+        };
+
+        trackpad = {
+            # Enable tap to click
+            Clicking = true;
+            # Enable two finger right click
+            TrackpadRightClick = true;
+            # Enable three finger drag
+            TrackpadThreeFingerDrag = true;
+        };
+
+        NSGlobalDomain = {
+            # Enable natural scrolling
+            "com.apple.swipescrolldirection" = true;
+            # Disable beep sound when pressing volume up/down key
+            "com.apple.sound.beep.feedback" = 0;
+            # Dark mode
+            AppleInterfaceStyle = "Dark";
+            # Enable full keyboard control
+            AppleKeyboardUIMode = 3;
+            # Disable auto capitalization
+            NSAutomaticCapitalizationEnabled = false;
+            # Disable auto dash substitution
+            NSAutomaticDashSubstitutionEnabled = false;
+            # Disable auto period substitution
+            NSAutomaticPeriodSubstitutionEnabled = false;
+            # Disable auto quote substitution
+            NSAutomaticQuoteSubstitutionEnabled = false;
+            # Disable auto spelling correction
+            NSAutomaticSpellingCorrectionEnabled = false;
+            # Expand save panel by default
+            NSNavPanelExpandedStateForSaveMode = true;
+            NSNavPanelExpandedStateForSaveMode2 = true;
+        };
+
+        keyboard = {
+            enableKeyMapping = true;
+            remapCapsLockToEscape  = true;
+        };
+    };
 
     # Do not need to update
     system.stateVersion = lib.mkForce 4;
