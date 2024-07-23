72339c2367ba6b978dab90381014595133f91fbf
jadc <chehimj@gmail.com>
2024-07-22 22:55:53 -0600

    zsh for all profiles

--- config/configuration.common.nix
+++ config/configuration.common.nix
@@ -1,7 +1,7 @@
 # The following configuration.nix is shared amongst all profiles
 # You shouldn't need to modify this file.
 
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
     imports = [ ./options.common.nix ];
@@ -14,6 +14,11 @@
 
     # Ensure nixpkgs serves binaries for the correct architecture
     nixpkgs.hostPlatform = config.common.arch;
+
+    # Set default shell to zsh for all users
+    programs.zsh.enable = true;
+    environment.shells = with pkgs; [ zsh ];
+    users.defaultUserShell = pkgs.zsh;
 
     # Do not need to update
     system.stateVersion = "24.05";
