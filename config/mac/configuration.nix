8cdf00457034fde9205d279069cf0a3888736b7a
jadc <chehimj@gmail.com>
2024-07-23 13:58:25 -0600

    wip

--- config/mac/configuration.nix
+++ config/mac/configuration.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 
 {
     imports =
@@ -10,81 +10,99 @@
             # System configuration
             #./../../system/users.nix
             #./../../system/nix-gc.nix
-            #./../../system/fonts.nix
         ];
 
     # Enable Nix daemon for macOS
     services.nix-daemon.enable = true;
 
+    # Fonts
+    /*
+    fonts.packages = with pkgs; [
+        (nerdfonts.override {
+            fonts = [
+                "NerdFontsSymbolsOnly"
+                "JetBrainsMono"
+            ];
+        })
+    ];
+    */
+
     # Enable homebrew packages
     homebrew = {
         enable = true;
         onActivation.cleanup = "uninstall";
+        casks = [
+            #discord
+            "google-chrome"
+            #jellyfin-media-player
+            #mactex
+            #moonlight
+            #obsidian
+        ];
     };
 
     # Allow Touch ID to grant superuser
     security.pam.enableSudoTouchIdAuth = true;
 
-    system.defaults = {
-        dock = {
-            autohide = true;
-            show-recents = false;
-        };
-
-        finder = {
-            # Show full path in finder title
-            _FXShowPosixPathInTitle = true;
-            # Show all file extensions
-            AppleShowAllExtensions = true;
-            # Disable warning when changing file extension
-            FXEnableExtensionChangeWarning = false;
-            # Enable quit menu item
-            QuitMenuItem = true;
-            # Show path bar
-            ShowPathbar = true;
-            # Show status bar
-            ShowStatusBar = true;
-        };
-
-        trackpad = {
-            # Enable tap to click
-            Clicking = true;
-            # Enable two finger right click
-            TrackpadRightClick = true;
-            # Enable three finger drag
-            TrackpadThreeFingerDrag = true;
-        };
-
-        NSGlobalDomain = {
-            # Enable natural scrolling
-            "com.apple.swipescrolldirection" = true;
-            # Disable beep sound when pressing volume up/down key
-            "com.apple.sound.beep.feedback" = 0;
-            # Dark mode
-            AppleInterfaceStyle = "Dark";
-            # Enable full keyboard control
-            AppleKeyboardUIMode = 3;
-            # Disable auto capitalization
-            NSAutomaticCapitalizationEnabled = false;
-            # Disable auto dash substitution
-            NSAutomaticDashSubstitutionEnabled = false;
-            # Disable auto period substitution
-            NSAutomaticPeriodSubstitutionEnabled = false;
-            # Disable auto quote substitution
-            NSAutomaticQuoteSubstitutionEnabled = false;
-            # Disable auto spelling correction
-            NSAutomaticSpellingCorrectionEnabled = false;
-            # Expand save panel by default
-            NSNavPanelExpandedStateForSaveMode = true;
-            NSNavPanelExpandedStateForSaveMode2 = true;
-        };
+    # Dock configuration
+    system.defaults.dock = {
+        tilesize = 48;
+        autohide = true;
+        show-recents = false;
+        minimize-to-application = true;
     };
 
-    system = {
-        keyboard = {
-            enableKeyMapping = true;
-            remapCapsLockToEscape  = true;
-        };
+    system.defaults.finder = {
+        # Show full path in finder title
+        _FXShowPosixPathInTitle = true;
+        # Show all file extensions
+        AppleShowAllExtensions = true;
+        # Disable warning when changing file extension
+        FXEnableExtensionChangeWarning = false;
+        # Enable quit menu item
+        QuitMenuItem = true;
+        # Show path bar
+        ShowPathbar = true;
+        # Show status bar
+        ShowStatusBar = true;
+    };
+
+    system.defaults.trackpad = {
+        # Enable tap to click
+        Clicking = true;
+        # Enable two finger right click
+        TrackpadRightClick = true;
+        # Enable three finger drag
+        TrackpadThreeFingerDrag = true;
+    };
+
+    system.defaults.NSGlobalDomain = {
+        # Enable natural scrolling
+        "com.apple.swipescrolldirection" = true;
+        # Disable beep sound when pressing volume up/down key
+        "com.apple.sound.beep.feedback" = 0;
+        # Dark mode
+        AppleInterfaceStyle = "Dark";
+        # Enable full keyboard control
+        AppleKeyboardUIMode = 3;
+        # Disable auto capitalization
+        NSAutomaticCapitalizationEnabled = false;
+        # Disable auto dash substitution
+        NSAutomaticDashSubstitutionEnabled = false;
+        # Disable auto period substitution
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        # Disable auto quote substitution
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        # Disable auto spelling correction
+        NSAutomaticSpellingCorrectionEnabled = false;
+        # Expand save panel by default
+        NSNavPanelExpandedStateForSaveMode = true;
+        NSNavPanelExpandedStateForSaveMode2 = true;
+    };
+
+    system.keyboard = {
+        enableKeyMapping = true;
+        remapCapsLockToEscape  = true;
     };
 
 
