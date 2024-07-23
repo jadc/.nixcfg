dfa0d85ee4223b5346312aa3790e80ec3cad7f81
jadc <chehimj@gmail.com>
2024-07-22 22:11:39 -0600

    moved uncommon

--- config/configuration.common.nix
+++ config/configuration.common.nix
@@ -1,31 +1,13 @@
 # The following configuration.nix is shared amongst all profiles
 # You shouldn't need to modify this file.
 
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
     imports = [ ./options.common.nix ];
 
     # Hostname
     networking.hostName = config.common.hostname;
-
-    # Setup locale
-    time.timeZone = config.common.timeZone;
-    i18n.defaultLocale = config.common.locale;
-    i18n.extraLocaleSettings = {
-        LC_ADDRESS = config.common.locale;
-        LC_IDENTIFICATION = config.common.locale;
-        LC_MEASUREMENT = config.common.locale;
-        LC_MONETARY = config.common.locale;
-        LC_NAME = config.common.locale;
-        LC_NUMERIC = config.common.locale;
-        LC_PAPER = config.common.locale;
-        LC_TELEPHONE = config.common.locale;
-        LC_TIME = config.common.locale;
-    };
-
-    # Rebuild won't work without git
-    environment.systemPackages = with pkgs; [ git ];
 
     # Enable flakes
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
