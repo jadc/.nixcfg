c0765fb705bd8157d50f6d7234240a673bc73f0d
jadc <chehimj@gmail.com>
2024-07-24 22:14:06 -0600

    nix-gc moved

--- system/nix-gc.nix
+++
@@ -1,8 +0,0 @@
-{
-    # Clean >= 30 day old generations every week
-    nix.gc = {
-        automatic = true;
-        options = "--delete-older-than 7d";
-        interval = [{ Hour = 21; Minute = 0; Weekday = 7; }];
-    };
-}
