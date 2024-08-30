b848b7cbaafb68009203d75cda2db292c7192c36
jadc <chehimj@gmail.com>
2024-08-30 16:34:58 -0600

    Misc

--- system/gc/default.nix
+++ system/gc/default.nix
@@ -1,17 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-    # Common
     nix.gc = {
         automatic = true;
         options = "--delete-older-than 7d";
     }
     // lib.optionalAttrs pkgs.stdenv.isLinux {
-        # NixOS
         dates = "weekly";
     }
     // lib.optionalAttrs pkgs.stdenv.isDarwin {
-        # nix-darwin
         user = config.common.username;
         interval = [{ Hour = 21; Minute = 0; Weekday = 7; }];
     };
