13417ba13267c2eb2d9e6bb9c88f00f7899ba620
jadc <chehimj@gmail.com>
2024-08-30 20:51:01 -0600

    Cleaner fix for vesktop config on mac

--- user/app/vesktop/default.nix
+++ user/app/vesktop/default.nix
@@ -1,18 +1,14 @@
-{ lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
     home.packages = with pkgs; [ vesktop ];
 
-    # Configuration
-    xdg.configFile.vesktop = lib.mkIf pkgs.stdenv.isLinux {
+    home.file.vesktop = let
+        configHome = if pkgs.stdenv.isDarwin
+            then "/Library/Application Support"
+            else "${config.xdg.configHome}";
+    in {
         source = ./settings.json;
-        target = "vesktop/settings/settings.json";
-    };
-
-    # If on macOS, symlink to where config is read on macOS
-    # /Users/jad/Library/Application Support/vesktop
-    home.file.vesktop = lib.mkIf pkgs.stdenv.isDarwin {
-        source = ./settings.json;
-        target = "Library/Application Support/vesktop/settings.json";
+        target = "${configHome}/vesktop/settings/settings.json";
     };
 }
