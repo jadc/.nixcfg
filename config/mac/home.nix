0f93e521c92cfaa6079bc72556e40e8179229615
jadc <chehimj@gmail.com>
2024-09-01 14:18:13 -0600

    CMPUT 415

--- config/mac/home.nix
+++ config/mac/home.nix
@@ -9,21 +9,21 @@
             ./../../user/app/kitty
             ./../../user/app/chrome
             ./../../user/app/vesktop
-            #./../../user/app/zathura
+            ./../../user/app/zathura
             ./../../user/app/spotify
             ./../../user/app/syncthing
-            #./../../user/app/deluge
+            ./../../user/app/deluge
             ./../../user/app/obsidian
+            ./../../user/app/minecraft
+            ./../../user/app/415
 
             ## Multimedia
-            #./../../user/app/audacity
-            #./../../user/app/spek
+            ./../../user/app/audacity
+            ./../../user/app/spek
             #./../../user/app/puddletag
             #./../../user/app/avidemux
-            #./../../user/app/gimp
-            #./../../user/app/inkscape
-            #./../../user/app/jellyfin-player
-            #./../../user/app/mpv
+            ./../../user/app/gimp
+            ./../../user/app/mpv
             #./../../user/app/obs
             ./../../user/cli/flac
             ./../../user/cli/exiftool
