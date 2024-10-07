5398a149ba932394ba5472b5414dd080d592179d
jadc <chehimj@gmail.com>
2024-10-07 08:41:34 -0600

    Resolved quote warning

--- user/scripts/rebuild.nix
+++ user/scripts/rebuild.nix
@@ -5,13 +5,13 @@ let
         name = "rebuild";
         text =
             if (pkgs.stdenv.isDarwin) then
-                "sudo -u \"$USER\" darwin-rebuild switch --flake \"$HOME/.nixcfg\"#${config.common.hostname}"
+                ''sudo -u "$USER" darwin-rebuild switch --flake "$HOME/.nixcfg"#${config.common.hostname}''
             else
                 ''
                 if grep -q "NixOS" /etc/os-release; then
-                    sudo nixos-rebuild switch --flake \"$HOME/.nixcfg\"#${config.common.hostname};
+                    sudo nixos-rebuild switch --flake "$HOME/.nixcfg"#${config.common.hostname};
                 else
-                    home-manager switch --flake \"$HOME/.nixcfg\"#${config.common.hostname};
+                    home-manager switch --flake "$HOME/.nixcfg"#${config.common.hostname};
                 fi
                 ''
             ;
