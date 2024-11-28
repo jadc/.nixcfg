d8ad6c331a2509aaf3a712a6dbecb29597f992cc
jadc <chehimj@gmail.com>
2024-11-28 09:15:51 -0700

    Tmux resize pane bind

--- user/cli/tmux/default.nix
+++ user/cli/tmux/default.nix
@@ -15,6 +15,9 @@
         terminal = "tmux-256color";
 
         extraConfig = ''
+            # unbind everything
+            unbind -a
+
             # split panes using | and -, in the current path
             bind \\ split-window -h -c "#{pane_current_path}"
             bind - split-window -v -c "#{pane_current_path}"
@@ -33,6 +36,12 @@
             bind -n M-l select-pane -R
             bind -n M-j select-pane -U
             bind -n M-k select-pane -D
+
+            # resize panes using Alt-shift-arrow without prefix
+            bind -n -r M-H resize-pane -L 5
+            bind -n -r M-L resize-pane -R 5
+            bind -n -r M-J resize-pane -U 5
+            bind -n -r M-K resize-pane -D 5
 
             # hide status bar
             set -g status off
