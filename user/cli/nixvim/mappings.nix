0c53ed12537ebaede0a7eb23d9bc87d0eafdee17
jadc <chehimj@gmail.com>
2025-02-22 16:59:02 -0700

    vim indent keep selection

--- user/cli/nixvim/mappings.nix
+++ user/cli/nixvim/mappings.nix
@@ -1,47 +1,19 @@
 {
     programs.nixvim.keymaps = [
         # Disable arrow keys
-        { 
-            key = "<Up>"; 
-            mode = [ "n" "x" "i" ];
-            action = "<Nop>"; 
-        }
-        { 
-            key = "<Down>"; 
-            mode = [ "n" "x" "i" ];
-            action = "<Nop>"; 
-        }
-        { 
-            key = "<Left>"; 
-            mode = [ "n" "x" "i" ];
-            action = "<Nop>"; 
-        }
-        { 
-            key = "<Right>"; 
-            mode = [ "n" "x" "i" ];
-            action = "<Nop>"; 
-        }
+        { key = "<Up>"; mode = [ "n" "x" "i" ]; action = "<Nop>"; }
+        { key = "<Down>"; mode = [ "n" "x" "i" ]; action = "<Nop>"; }
+        { key = "<Left>"; mode = [ "n" "x" "i" ]; action = "<Nop>"; }
+        { key = "<Right>"; mode = [ "n" "x" "i" ]; action = "<Nop>"; }
 
         # Allow movement through wrapped lines
-        { 
-            key = "j"; 
-            mode = [ "n" "x" ];
-            action = "gj"; 
-        }
-        { 
-            key = "k"; 
-            mode = [ "n" "x" ];
-            action = "gk"; 
-        }
-        {
-            key = "$";
-            mode = [ "n" "x" ];
-            action = "g$";
-        }
-        {
-            key = "0";
-            mode = [ "n" "x" ];
-            action = "g0";
-        }
+        { key = "j"; mode = [ "n" "x" ]; action = "gj"; }
+        { key = "k"; mode = [ "n" "x" ]; action = "gk"; }
+        { key = "$"; mode = [ "n" "x" ]; action = "g$"; }
+        { key = "0"; mode = [ "n" "x" ]; action = "g0"; }
+
+        # Keep selection after indent
+        { key = "<"; mode = [ "v" ]; action = "<gv"; }
+        { key = ">"; mode = [ "v" ]; action = ">gv"; }
     ];
 }
