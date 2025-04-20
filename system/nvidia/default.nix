fca1e7397304fca95c06b1a89240ace801bccfe4
jadc <chehimj@gmail.com>
2025-04-19 19:35:25 -0600

    Use open nvidia drivers

--- system/nvidia/default.nix
+++ system/nvidia/default.nix
@@ -1,14 +1,21 @@
-# To support nVidia dedicated graphics
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
+    # Enable OpenGL
     hardware.graphics.enable = true;
+
+    # Load driver for Xorg and Wayland
     services.xserver.videoDrivers = [ "nvidia" ];
     boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+
     hardware.nvidia = {
         modesetting.enable = true;
+
+        # Use open drivers (for modern cards)
+        open = true;
+
+        # Enable settings menu (nvidia-settings)
         nvidiaSettings = true;
-        open = false;
 
         # Use beta channel
         package = config.boot.kernelPackages.nvidiaPackages.beta;
