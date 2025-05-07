d5537f0e29c66f3b19ecbd3619694273f4ffad35
jadc <chehimj@gmail.com>
2025-05-06 21:22:07 -0400

    required line for droidcam

--- system/droidcam/default.nix
+++ system/droidcam/default.nix
@@ -11,10 +11,7 @@
     boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
     boot.kernelModules = ["v4l2loopback"];
 
-    /*
     boot.extraModprobeConfig = ''
-        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+        options v4l2loopback devices=1 video_nr=1 card_label="DroidCam" exclusive_caps=1
     '';
-    security.polkit.enable = true;
-    */
 }
