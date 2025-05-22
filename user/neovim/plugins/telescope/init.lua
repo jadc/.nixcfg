d1e41b4944d62e0ceca7997482236cdb73f1ed6e
jadc <chehimj@gmail.com>
2025-05-21 22:27:21 -0400

    Larger telescope

--- user/neovim/plugins/telescope/init.lua
+++ user/neovim/plugins/telescope/init.lua
@@ -1,19 +1,33 @@
-require("telescope").setup({})
+require("telescope").setup({
+    defaults = {
+        border = false,
+        layout_strategy = "vertical",
+        sorting_strategy = "ascending",
+        layout_config = {
+            vertical = {
+                height = 0.999,
+                width = 0.999,
+                prompt_position = "top",
+            },
+        },
+    },
+})
 
+local ts = require("telescope.builtin")
 local is_inside_work_tree = {}
 _G.project_files = function()
-    local opts = {}
     local cwd = vim.fn.getcwd()
     if is_inside_work_tree[cwd] == nil then
         vim.fn.system("git rev-parse --is-inside-work-tree")
         is_inside_work_tree[cwd] = vim.v.shell_error == 0
     end
     if is_inside_work_tree[cwd] then
-        require("telescope.builtin").git_files(opts)
+        ts.git_files()
     else
-        require("telescope.builtin").find_files(opts)
+        ts.find_files()
     end
 end
 
 vim.keymap.set({ "n", "x" }, "<c-f>", "<cmd>Telescope live_grep<cr>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "x" }, "<c-p>", "<cmd>lua project_files()<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "x" }, "<c-P>", ts.find_files, { noremap = true, silent = true })
