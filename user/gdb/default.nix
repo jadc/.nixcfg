21c7aa159515ea932dec0f6180081b48b4f225d6
jadc <chehimj@gmail.com>
2025-06-04 09:05:23 -0400

    tweak gdb

--- user/gdb/default.nix
+++ user/gdb/default.nix
@@ -2,5 +2,8 @@
 
 {
     home.packages = with pkgs; [ gdb ];
-    common.aliases.gdb = "${pkgs.gdb}/bin/gdb --tui $@";
+    common.aliases.gdb = "${pkgs.gdb}/bin/gdb --tui --args $@";
+
+    # Disable path security check
+    home.file.".gdbinit".text = "set auto-load safe-path /";
 }
