23665dce6aecd051fb70c7962a762e890d15ec42
jadc <chehimj@gmail.com>
2025-06-29 22:06:52 -0400

    tlp

--- config/laptop/configuration.nix
+++ config/laptop/configuration.nix
@@ -22,6 +22,7 @@
             ./../../system/networkmanager
             ./../../system/sound
             ./../../system/swapfile
+            ./../../system/tlp
             ./../../system/trim
             ./../../system/users
 
