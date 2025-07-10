ca31abc97f16a8501e3d7ee75b05ffaf9d75dfe7
jadc <chehimj@gmail.com>
2025-07-10 17:19:38 -0600

    whisper alias

--- user/whisper/gpu.nix
+++ user/whisper/gpu.nix
@@ -2,4 +2,5 @@
 
 {
     home.packages = with pkgs; [ openai-whisper ];
+    common.aliases.whisper = "${pkgs.openai-whisper}/bin/whisper --model turbo --output_format txt --task transcribe --language en $@";
 }
