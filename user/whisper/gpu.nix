d9b8ec38f7f760afe3aa5caf1755374b2c412d5c
jadc <chehimj@gmail.com>
2025-07-10 20:22:46 -0400

    better whisper model

--- user/whisper/gpu.nix
+++ user/whisper/gpu.nix
@@ -2,5 +2,5 @@
 
 {
     home.packages = with pkgs; [ openai-whisper ];
-    common.aliases.whisper = "${pkgs.openai-whisper}/bin/whisper --model turbo --output_format txt --task transcribe --language en $@";
+    common.aliases.whisper = "${pkgs.openai-whisper}/bin/whisper --model large-v3 --output_format txt --task transcribe --language en $@";
 }
