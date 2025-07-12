2671b7dbe3d0d2a9415cb224def855a77cbabc6b
jadc <chehimj@gmail.com>
2025-07-11 19:00:00 -0600

    temp

--- user/envs/whisper/shell.nix
+++ user/envs/whisper/shell.nix
@@ -1,19 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-    buildInputs = [
-        (pkgs.whisper-cpp.override { cudaSupport = true; })
-        pkgs.cudaPackages.cuda_cudart
-        pkgs.cudaPackages.cuda_nvcc
-        pkgs.cudaPackages.cudatoolkit
-    ];
-
-    shellHook = ''
-        export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
-        export PATH=$CUDA_PATH/bin:$PATH
-        export LIBRARY_PATH=$CUDA_PATH/lib:$LIBRARY_PATH
-        export LD_LIBRARY_PATH=$CUDA_PATH/lib:/run/opengl-driver/lib:/run/opengl-driver-32:$LD_LIBRARY_PATH
-
-        alias whisper="whisper --model large-v3 --output_format txt --task transcribe --language en"
-    '';
+    buildInputs = with pkgs; [ whisper-cpp ];
 }
