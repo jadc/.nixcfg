e22cc968ffa8d9501b85854b99eeed2ad7195df7
jadc <chehimj@gmail.com>
2025-07-11 19:03:33 -0600

    give up (for now 2)

--- user/envs/whisper/shell.nix
+++ user/envs/whisper/shell.nix
@@ -2,4 +2,7 @@
 
 pkgs.mkShell {
     buildInputs = with pkgs; [ openai-whisper ];
+    shellHook = ''
+        alias whisper="whisper --model large-v3 --output_format txt --task transcribe --language en"
+    '';
 }
