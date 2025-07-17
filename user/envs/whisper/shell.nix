5e2f920164247162024abf5ac7f922459b4129b0
jadc <chehimj@gmail.com>
2025-07-17 17:12:07 -0600

    fix: working whisper ai with cuda

--- user/envs/whisper/shell.nix
+++ user/envs/whisper/shell.nix
@@ -1,8 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-    buildInputs = with pkgs; [ openai-whisper ];
+    buildInputs = with pkgs; [
+        openai-whisper
+        cudaPackages.cuda_cudart
+        cudaPackages.cuda_nvcc
+        cudaPackages.cudatoolkit
+    ];
+
     shellHook = ''
-        alias whisper="whisper --model large-v3 --output_format txt --task transcribe --language en"
+        export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+        export PATH=$CUDA_PATH/bin:$PATH
+        export LIBRARY_PATH=$CUDA_PATH/lib:$LIBRARY_PATH
+        export LD_LIBRARY_PATH=$CUDA_PATH/lib:/run/opengl-driver/lib:/run/opengl-driver-32:$LD_LIBRARY_PATH
     '';
 }
