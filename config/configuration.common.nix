7b86bc77ea7dbde98041ea0f12454eb9c4450d34
jadc <chehimj@gmail.com>
2025-07-21 09:07:36 -0400

    tweak: moved cachix to nixos only

--- config/configuration.common.nix
+++ config/configuration.common.nix
@@ -12,6 +12,11 @@
 
         # Enable flakes
         experimental-features = [ "nix-command" "flakes" ];
+
+        # Use pre-compiled binaries from nix-community cache if available
+        substituters = [ "https://nix-community.cachix.org" ];
+        trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+        trusted-users = [ "@wheel" ];
     };
 
     # Set default shell to zsh for all users
