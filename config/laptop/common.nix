85dc879251fd54358369cb22c776feded733bc6e
jadc <chehimj@gmail.com>
2025-07-22 16:27:56 -0400

    refactor: consolidated aliases

--- config/laptop/common.nix
+++ config/laptop/common.nix
@@ -3,22 +3,5 @@
         profile = "laptop";
         arch = "x86_64-linux";
         username = "jad";
-
-        aliases = {
-            # Default flags
-            sudo="sudo -E";
-            du="du -h";
-            cp="cp -ivp";
-            mv="mv -iv";
-            mkdir="mkdir -pv";
-            make="make -j$(nproc)";
-
-            ## Colors
-            # ls="ls -hN --color=auto --group-directories-first";
-            grep="grep --color=auto";
-            fgrep="fgrep --color=auto";
-            egrep="egrep --color=auto";
-            diff="diff --color=auto";
-        };
     };
 }
