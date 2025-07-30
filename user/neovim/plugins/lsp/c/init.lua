62bd5e3a80ee6b8da69ab494caf8468ca6681561
jadc <chehimj@gmail.com>
2025-07-30 14:56:07 -0400

    tweak: clangd edits

--- user/neovim/plugins/lsp/c/init.lua
+++ user/neovim/plugins/lsp/c/init.lua
@@ -1,5 +1,14 @@
 setup_server(
     "clangd", {
-        cmd = { "clangd", "--background-index", "--clang-tidy" },
+        cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--limit-results=0",
+            "--log=verbose"
+        },
+        init_options = {
+            fallbackFlags = { "-std=c++11" },
+        },
     }
 )
--- user/neovim/plugins/treesitter/default.nix
+++ user/neovim/plugins/treesitter/default.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 
 {
-    home.packages = with pkgs; [ gcc ];
     programs.neovim.plugins = with pkgs.vimPlugins; [ nvim-treesitter.withAllGrammars ];
     xdg.configFile."nvim/after/plugin/treesitter.lua".source = ./init.lua;
 }
