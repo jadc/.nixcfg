b05fa096c09993e0778256eaa3429c4b896f636d
jadc <chehimj@gmail.com>
2026-03-11 22:27:58 -0600

    Add cap to number of saved NixOS generations

--- cfg/modules/systemd-boot/default.nix
+++ cfg/modules/systemd-boot/default.nix
@@ -12,6 +12,7 @@ in
     config = lib.mkIf self.enable {
         boot.loader = {
             systemd-boot.enable = true;
+            systemd-boot.configurationLimit = 5;
             efi.canTouchEfiVariables = true;
             timeout = 0;
         };
