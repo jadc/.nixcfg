537aef256ba4a464be92582643e9141576f61f99
jadc <chehimj@gmail.com>
2026-05-04 15:54:17 -0600

    Add some more modules to home profile

--- profiles/home/default.nix
+++ profiles/home/default.nix
@@ -5,18 +5,28 @@ let
 
     profile = {
         cfg = {
+            # Setup
+            ssh.enable = true;
+            xdg.enable = true;
+
+            # Command-line Interface
             archivers.enable = true;
             bat.enable = true;
             claude-code.enable = true;
             direnv.enable = true;
             envs.enable = true;
+            exiftool.enable = true;
             eza.enable = true;
+            ffmpeg.enable = true;
             fzf.enable = true;
             gdb.enable = true;
             git.enable = true;
             htop.enable = true;
             hyperfine.enable = true;
+            imagemagick.enable = true;
+            poppler.enable = true;
             ripgrep.enable = true;
+            rsync.enable = true;
             tmux.enable = true;
             zoxide.enable = true;
             zsh.enable = true;
