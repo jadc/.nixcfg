c84c172ce92fa49ddbaeeaf840c66a614ca8a70f
jadc <chehimj@gmail.com>
2026-05-13 15:20:36 -0600

    Enable xwayland in niri

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -43,6 +43,7 @@ in
     flake.modules.homeManager.${name} = { config, lib, pkgs, ... }: let self = config.cfg.${name}; in {
         config = lib.mkIf self.enable {
             home.packages = with pkgs; [
+                xwayland-satellite
                 wl-clipboard
                 brightnessctl
                 playerctl
