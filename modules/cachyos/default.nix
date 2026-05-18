2640457db167353214049bc62a431439971ad151
jadc <chehimj@gmail.com>
2026-05-18 13:42:09 -0600

    Use pinned overlay to actually hit cachyos cached builds

--- modules/cachyos/default.nix
+++ modules/cachyos/default.nix
@@ -12,12 +12,18 @@ in
 
     flake.modules.nixos.${name} = { config, inputs, lib, pkgs, ... }: let self = config.cfg.${name}; in {
         config = lib.mkIf self.enable {
-            nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.default ];
+            nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
             cfg.kernel.build = lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-latest;
 
             nix.settings = {
-                substituters = [ "https://cache.garnix.io" ];
-                trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+                substituters = [
+                    "https://attic.xuyh0120.win/lantian"
+                    "https://cache.garnix.io"
+                ];
+                trusted-public-keys = [
+                    "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+                    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+                ];
             };
         };
     };
