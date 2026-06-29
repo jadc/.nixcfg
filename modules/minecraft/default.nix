e57ab112cc4e0ca29aa5d3467f238dbbd9e80540
jadc <chehimj@gmail.com>
2026-06-28 18:55:34 -0600

    Switch from NixOS to Temurin JDK

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -13,27 +13,19 @@ in
     flake.modules.homeManager.${name} = { config, lib, pkgs, ... }:
     let
         self = config.cfg.${name};
-        alternative = [ pkgs.jdk8 pkgs.jdk17 ];
     in {
         config = lib.mkIf self.enable {
-            home.packages = [ pkgs.prismlauncher ];
-
-            # Java
-            programs.java = {
-                enable = true;
-
-                # Default java is latest
-                package = pkgs.jdk;
-            };
-
-            # Alternative java versions
-            home.file = (builtins.listToAttrs (map (jdk: {
-                name = ".jdks/${jdk.version}";
-                value = { source = jdk; };
-            }) alternative));
+            home.packages = [
+                (pkgs.prismlauncher.override {
+                    jdks = [
+                        pkgs.temurin-bin-8
+                        pkgs.temurin-bin-17
+                        pkgs.temurin-bin
+                    ];
+                })
+            ];
 
             home.sessionVariables = {
-                _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
                 _JAVA_AWT_WM_NONREPARENTING = 1;
             };
 
