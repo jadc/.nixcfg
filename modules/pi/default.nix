f669d8535b2bf00d5a7fa21c7f2e2ff56e44da94
jadc <chehimj@gmail.com>
2026-08-17 11:23:57 -0400

    Copy pi config so it can be written at run-time

--- modules/pi/default.nix
+++ modules/pi/default.nix
@@ -15,45 +15,16 @@ in
             programs.pi-coding-agent = {
                 enable = true;
                 extraPackages = [ pkgs.nodejs_latest ];
-                settings = {
-                    theme = "gh-dark";
-
-                    packages = [
-                        # Theme
-                        "npm:pi-gh-dark-theme"
-
-                        # Extensions
-                        "npm:@tintinweb/pi-subagents"
-                        "npm:pi-lens"
-                        "npm:pi-web-access"
-                        {
-                            source = "npm:mattpocock-skills";
-                            skills = [ "skills/productivity/grill-me/SKILL.md" ];
-                        }
-                    ];
-
-                    skills = let home = config.home.homeDirectory; in [
-                        "${home}/.claude/skills"
-                        "${home}/.codex/skills"
-                    ];
-
-                    # Disable telemetry.
-                    enableInstallTelemetry = false;
-                    enableAnalytics = false;
-
-                    # UI Behaviour
-                    tuiMode = "fullscreen";
-                    terminal = {
-                        # Clear empty rows when content shrinks; can cause flicker.
-                        clearOnShrink = true;
-
-                        # Show OSC 9;4 progress indicators in the terminal tab bar.
-                        showTerminalProgress = true;
-                    };
-                };
             };
 
-            cfg.save.home.dirs = [ ".pi/agent" ];
+            # Copy configuration as r+w without deleting files created by pi.
+            home.activation.piFiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+                run ${pkgs.rsync}/bin/rsync \
+                    -a --chmod=Du+w,Fu+w --checksum --no-owner --no-group \
+                    ${./config}/ "$HOME/"
+            '';
+
+            cfg.save.home.dirs = [ ".pi" ];
         };
     };
 }
