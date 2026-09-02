01dd5dd59906734c40c7273888b3473510c3578f
jadc <chehimj@gmail.com>
2026-09-02 18:52:09 -0400

    .

--- modules/style/default.nix
+++ modules/style/default.nix
@@ -1,7 +1,41 @@
+# Original custom scheme:
+# base00 = "000000"; base01 = "252525"; base02 = "535353"; base03 = "505050";
+# base04 = "7fc8ff"; base05 = "ffffff"; base06 = "ffffff"; base07 = "ffffff";
+# base08 = "ff009e"; base09 = "ff00d4"; base0A = "fffd00"; base0B = "00ffa2";
+# base0C = "00ffea"; base0D = "00e0ff"; base0E = "00efff"; base0F = "f274bc";
+
 { inputs, ... }:
 
 {
-    flake.modules.nixos.stylix = { pkgs, ... }: {
+    flake.modules.generic.style = { lib, ... }:
+    let
+        color = default: lib.mkOption {
+            type = lib.types.str;
+            inherit default;
+        };
+    in
+    {
+        options.cfg.style.colors = {
+            base00 = color "#000000";
+            base01 = color "#282828";
+            base02 = color "#585858";
+            base03 = color "#888888";
+            base04 = color "#c8c8c8";
+            base05 = color "#ffffff";
+            base06 = color "#ffffff";
+            base07 = color "#ffffff";
+            base08 = color "#fa7883";
+            base09 = color "#ffc387";
+            base0A = color "#ff9470";
+            base0B = color "#98c379";
+            base0C = color "#8af5ff";
+            base0D = color "#6bb8ff";
+            base0E = color "#e799ff";
+            base0F = color "#b3684f";
+        };
+    };
+
+    flake.modules.nixos.style = { pkgs, ... }: {
         imports = [ inputs.stylix.nixosModules.stylix ];
 
         config.stylix = {
@@ -11,12 +45,6 @@
                 applications = 0.85;
                 terminal = 0.85;
             };
-
-            # Original custom scheme:
-            # base00 = "000000"; base01 = "252525"; base02 = "535353"; base03 = "505050";
-            # base04 = "7fc8ff"; base05 = "ffffff"; base06 = "ffffff"; base07 = "ffffff";
-            # base08 = "ff009e"; base09 = "ff00d4"; base0A = "fffd00"; base0B = "00ffa2";
-            # base0C = "00ffea"; base0D = "00e0ff"; base0E = "00efff"; base0F = "f274bc";
 
             # Theme
             polarity = "dark";
