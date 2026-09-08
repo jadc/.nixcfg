24396c794d4121ebce12292eb782202f4c51d499
jadc <chehimj@gmail.com>
2026-09-08 17:21:41 -0400

    Use only networkmanager to manage DHCP

--- profiles/desktop/hardware-configuration.nix
+++ profiles/desktop/hardware-configuration.nix
@@ -46,11 +46,8 @@
 
   swapDevices = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  # NetworkManager manages DHCP and routing for all interfaces.
+  networking.useDHCP = lib.mkForce false;
   networking.interfaces.enp4s0.wakeOnLan.enable = true;
   networking.interfaces.wlp3s0.wakeOnLan.enable = true;
 
--- profiles/laptop/hardware-configuration.nix
+++ profiles/laptop/hardware-configuration.nix
@@ -40,11 +40,8 @@
 
   swapDevices = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  # NetworkManager manages DHCP and routing for all interfaces.
+  networking.useDHCP = lib.mkForce false;
   # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
