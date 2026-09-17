f156e2ccfba42b11e982288d8dff9292578065c6
jadc <chehimj@gmail.com>
2026-09-17 19:16:42 -0400

    Reduce substituter retry attempts and delay

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -17,6 +17,10 @@
             substituters = [ "https://nix-community.cachix.org" ];
             trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
             trusted-users = [ "@wheel" ];
+
+            # Skip substituters if they're offline sooner
+            download-attempts = 3;
+            connect-timeout = 10;
         };
 
         nix.gc = {
